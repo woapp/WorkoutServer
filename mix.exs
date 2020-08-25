98397683ee9a417bf80fa2dfe650c2f4d4d61cfc
MattAgn <matthieua@bam.tech>
2020-08-25 07:16:53 +0200

    :heavy_plus_sign: add guardian for authentication

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule WorkoutServer.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.4"},
       {:absinthe_plug, "~> 1.4"},
-      {:poison, "~> 2.1.0"}
+      {:poison, "~> 2.1.0"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
