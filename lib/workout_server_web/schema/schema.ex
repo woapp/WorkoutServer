a3b7c46d357c28e9eda2b469271676175959b3d6
MattAgn <matthieua@bam.tech>
2020-09-09 00:51:29 +0200

    :sparkles: (schema) login query returns jwt

--- lib/workout_server_web/schema/schema.ex
+++ lib/workout_server_web/schema/schema.ex
@@ -17,19 +17,23 @@ defmodule WorkoutServerWeb.Schema.Schema do
     field :email, non_null(:string)
   end
 
+  object :session do
+    field :jwt, non_null(:string)
+  end
+
   query do
     @desc "Get all links"
     field :all_links, non_null(list_of(non_null(:link))) do
       resolve(&News.all_links/3)
     end
 
-    # @desc "Login with email/password"
-    # field :login, type: :session do
-    #   arg(:email, non_null(:string))
-    #   arg(:password, non_null(:string))
+    @desc "Login with email/password"
+    field :login, :session do
+      arg(:email, non_null(:string))
+      arg(:password, non_null(:string))
 
-    #   resolve(&UserResolver.login/2)
-    # end
+      resolve(&User.login/2)
+    end
   end
 
   mutation do
